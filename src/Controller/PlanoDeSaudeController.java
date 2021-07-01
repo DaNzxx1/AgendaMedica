059f5452a7f5000f8c7e920d4fcf32bbef88f60b
Ice <Ice@DESKTOP-VG6FG67>
2021-07-01 10:32:36 -0300

    Gerar Relatorio no Plano de Saude

--- src/Controller/PlanoDeSaudeController.java
+++ src/Controller/PlanoDeSaudeController.java
@@ -2,13 +2,30 @@ package Controller;
 
 import Entidades.PlanoDeSaude;
 import Persistencia.PlanoDeSaudeDAO;
+import com.lowagie.text.Cell;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Table;
+import com.lowagie.text.pdf.PdfWriter;
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.swing.JOptionPane;
 
 public class PlanoDeSaudeController {
+
     StringBuilder mensagensDeErro = new StringBuilder();
     PlanoDeSaudeDAO planoDeSaudeDao = new PlanoDeSaudeDAO();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     
     public List<PlanoDeSaude> listarTodosPlanoDeSaude() throws SQLException {
 
@@ -128,4 +145,150 @@ public class PlanoDeSaudeController {
         
         return true;
     }
+    
+        public void gerarRelatorioPlanoDeSaude() {
+        
+        //Instancia do documento e definindo o tamanha dela
+        Document documento = new Document(PageSize.A4.rotate());
+        
+        //Defina as margens
+        documento.setMargins(40f, 40f, 150f, 40f);
+        
+        try {
+            CabecalhoRodapeEvento inserircabecalho = new CabecalhoRodapeEvento();
+            
+            //Cria um arquivo relatorio.pdf com o documento
+            PdfWriter.getInstance(documento, new FileOutputStream("relatorioPlanoDeSaude.pdf")).setPageEvent(inserircabecalho);
+            
+            documento.open();
+            
+            Table tabelaPlanosDeSaude = new Table(6);
+            tabelaPlanosDeSaude.setBorder(10);
+            tabelaPlanosDeSaude.setBorderWidth(2);
+            tabelaPlanosDeSaude.setBorderColor(Color.black);
+            tabelaPlanosDeSaude.setPadding(2);
+            //tabelaUsuarios.setSpacing(0);
+            tabelaPlanosDeSaude.setWidth(100f);
+            tabelaPlanosDeSaude.setWidths(new float[]{10f, 30f, 20f, 10f, 15f, 15f});
+            
+            Paragraph paragrafoId = new Paragraph(new Phrase(12F, "ID",
+                    FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10F)));
+            paragrafoId.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph paragrafoCodigoPlano = new Paragraph(new Phrase(12F, "CODIGO_PLANO",
+                    FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10F)));
+            paragrafoCodigoPlano.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph paragrafoOperadora = new Paragraph(new Phrase(12F, "OPERADORA",
+                    FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10F)));
+            paragrafoOperadora.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph paragrafoTelefone = new Paragraph(new Phrase(12F, "TELEFONE",
+                    FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10F)));
+            paragrafoTelefone.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph paragrafoEndereco = new Paragraph(new Phrase(12F, "ENDERECO",
+                    FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10F)));
+            paragrafoEndereco.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph paragrafoRegistroANS = new Paragraph(new Phrase(12F, "REGISTRO_ANS",
+                    FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10F)));
+            paragrafoRegistroANS.setAlignment(Element.ALIGN_CENTER);
+            
+            Cell celulaPlanoDeSaudeId = new Cell(paragrafoId);
+            Cell celulaPlanoDeSaudeCodigoPlano = new Cell(paragrafoCodigoPlano);
+            Cell celulaPlanoDeSaudeOperadora = new Cell(paragrafoOperadora);
+            Cell celulaPlanoDeSaudeTelefone = new Cell(paragrafoTelefone);
+            Cell celulaPlanoDeSaudeEndereco = new Cell(paragrafoEndereco);
+            Cell celulaPlanoDeSaudeRegistroANS = new Cell(paragrafoRegistroANS);
+            
+            celulaPlanoDeSaudeId.setHeader(true);
+            tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeId);
+            tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeCodigoPlano);
+            tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeOperadora);
+            tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeTelefone);
+            tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeEndereco);
+            tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeRegistroANS);
+            
+            //definindo alinhamento, cores do cabeçalho da tabela
+            for (int i = 0; i < tabelaPlanosDeSaude.getColumns(); i++) {
+
+                Cell celula = (Cell) tabelaPlanosDeSaude.getElement(0, i);
+                celula.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celula.setBackgroundColor(Color.yellow);
+                celula.setBorderColor(Color.black);
+
+            }
+            
+            for(PlanoDeSaude planoDeSaude : listarTodosPlanosDeSaude()) {
+                paragrafoId = new Paragraph(new Phrase(15F, ""+ planoDeSaude.getId(),
+                    FontFactory.getFont(FontFactory.HELVETICA, 9F)));
+                paragrafoId.setAlignment(Element.ALIGN_CENTER);
+                celulaPlanoDeSaudeId = new Cell(paragrafoId);
+                celulaPlanoDeSaudeId.setBorderColor(Color.black);
+                celulaPlanoDeSaudeId.setHorizontalAlignment(Element.ALIGN_CENTER);
+                tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeId);
+                
+                paragrafoCodigoPlano = new Paragraph(new Phrase(15F, ""+ planoDeSaude.getCodigoPlano(),
+                    FontFactory.getFont(FontFactory.HELVETICA, 9F)));
+                paragrafoCodigoPlano.setAlignment(Element.ALIGN_CENTER);
+                celulaPlanoDeSaudeCodigoPlano = new Cell(paragrafoCodigoPlano);
+                celulaPlanoDeSaudeCodigoPlano.setBorderColor(Color.black);
+                celulaPlanoDeSaudeCodigoPlano.setHorizontalAlignment(Element.ALIGN_LEFT);
+                tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeCodigoPlano);
+                
+                paragrafoOperadora = new Paragraph(new Phrase(15F, ""+ planoDeSaude.getOperadora(),
+                    FontFactory.getFont(FontFactory.HELVETICA, 9F)));
+                paragrafoOperadora.setAlignment(Element.ALIGN_CENTER);
+                celulaPlanoDeSaudeOperadora = new Cell(paragrafoOperadora);
+                celulaPlanoDeSaudeOperadora.setBorderColor(Color.black);
+                celulaPlanoDeSaudeOperadora.setHorizontalAlignment(Element.ALIGN_CENTER);
+                tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeOperadora);
+                
+                paragrafoTelefone = new Paragraph(new Phrase(15F, ""+ planoDeSaude.getTelefone(),
+                    FontFactory.getFont(FontFactory.HELVETICA, 9F)));
+                paragrafoTelefone.setAlignment(Element.ALIGN_CENTER);
+                celulaPlanoDeSaudeTelefone = new Cell(paragrafoTelefone);
+                celulaPlanoDeSaudeTelefone.setBorderColor(Color.black);
+                celulaPlanoDeSaudeTelefone.setHorizontalAlignment(Element.ALIGN_CENTER);
+                tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeTelefone);
+                
+                paragrafoEndereco = new Paragraph(new Phrase(15F, ""+ planoDeSaude.getEndereco(),
+                    FontFactory.getFont(FontFactory.HELVETICA, 9F)));
+                paragrafoEndereco.setAlignment(Element.ALIGN_CENTER);
+                celulaPlanoDeSaudeEndereco = new Cell(paragrafoEndereco);
+                celulaPlanoDeSaudeEndereco.setBorderColor(Color.black);
+                celulaPlanoDeSaudeEndereco.setHorizontalAlignment(Element.ALIGN_CENTER);
+                tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeEndereco);
+                
+                paragrafoRegistroANS = new Paragraph(new Phrase(15F, ""+planoDeSaude.getRegistroANS(),
+                    FontFactory.getFont(FontFactory.HELVETICA, 9F)));
+                paragrafoRegistroANS.setAlignment(Element.ALIGN_CENTER);
+                celulaPlanoDeSaudeRegistroANS = new Cell(paragrafoRegistroANS);
+                celulaPlanoDeSaudeRegistroANS.setBorderColor(Color.black);
+                celulaPlanoDeSaudeRegistroANS.setHorizontalAlignment(Element.ALIGN_CENTER);
+                tabelaPlanosDeSaude.addCell(celulaPlanoDeSaudeRegistroANS);
+                
+            }    
+            
+            documento.add(tabelaPlanosDeSaude);
+            
+            Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c", "start", "relatorioMedico.pdf"});
+            
+            documento.close();
+        
+        } catch (DocumentException ex) {
+            System.out.println(ex);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }        
+    }
+        
+    private List<PlanoDeSaude> listarTodosPlanosDeSaude() throws SQLException {
+        return planoDeSaudeDao.listarTodos();
+    }    
 }
